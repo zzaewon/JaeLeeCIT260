004435e3297716681b776925d5dafa2c78686313
zzaewon <zzaewon@hotmail.com>
2014-04-25 15:37:34 -0600

    Update GaltToLitTable.java

--- GaltToLitTable.java
+++ GaltToLitTable.java
@@ -15,10 +15,10 @@ class GalToLitTable {
 			liters = gallons * 3.7854;	// convert to liters
 			System.out.println(gallons + " gallons is " + liters + " liters.");
 		
-			counter++;	// Increment the line counter with each loop iteration
+			counter++;			// Increment the line counter with each loop iteration
 		
 			// every 10th line, print a blank line
-			if (counter == 10) {	// if counter is 10, output a blank line
+			if (counter == 10) {		// if counter is 10, output a blank line
 				System.out.println();
 				counter = 0;		// reset the line counter
 			}
